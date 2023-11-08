b75d50fc4aa86e8eec2dae58063c6591289889fa
s-watanabe <s-watanabe@three-rings.net>
2023-11-08 14:28:34 +0900

    メインUI作成開始

---
+++ src/components/Register.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+// defineProps<{ msg: string }>()
+
+const APP_TITLE = ref(import.meta.env.VITE_APP_TITLE);
+</script>
+
+<template>
+  <dev>{{ APP_TITLE }}</dev>
+</template>
