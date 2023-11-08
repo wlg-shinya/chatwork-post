615b6f3a5dc19512b012cf66c2a4f6b66e3825b8
s-watanabe <s-watanabe@three-rings.net>
2023-11-08 14:20:16 +0900

    .envを利用できるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const htmlPlugin = () => ({
+    name: "html-transform",
+    transformIndexHtml: (html: string) => html.replace(/%=(.*?)%/g, (match, p1) => env[p1] ?? match),
+  });
+
+  return defineConfig({
+    plugins: [vue(), htmlPlugin()],
+  });
+};
