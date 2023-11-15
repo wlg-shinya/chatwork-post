eafe495171511ddae29c05245c136ea12dc685a8
s-watanabe <s-watanabe@three-rings.net>
2023-11-15 10:24:13 +0900

    データを登録できるように

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,6 +4,11 @@ import axios from "axios";
 
 // defineProps<{ msg: string }>()
 
+interface InputData {
+  roomInfo: string;
+  body: string;
+  selfUnread: boolean;
+}
 interface RegisteredData {
   id: number;
   roomId: number;
@@ -11,34 +16,28 @@ interface RegisteredData {
   selfUnread: boolean;
 }
 
+const inputData = ref<InputData>({
+  roomInfo: "https://www.chatwork.com/#!rid335028121",
+  body: import.meta.env.VITE_APP_TITLE,
+  selfUnread: false,
+});
 const registeredData = ref<RegisteredData[]>([]);
-const roomUrl = ref("https://www.chatwork.com/#!rid335028121");
-const body = ref(import.meta.env.VITE_APP_TITLE);
 
-const roomId = computed(() => roomUrl.value.replace(/.*rid([0-9]+)$/, "$1"));
+const roomId = computed((): number => {
+  const roomInfo = inputData.value.roomInfo;
+  if (roomInfo.startsWith("https://www.chatwork.com/#!rid")) {
+    // チャット部屋のURLを指定していた場合はID部分を抽出
+    return Number(roomInfo.replace(/.*rid([0-9]+)$/, "$1"));
+  } else if (!roomInfo.match(/[^0-9]+/)) {
+    // 部屋IDを直接指定していたらそのまま扱う
+    return Number(roomInfo);
+  } else {
+    throw new Error('Error: Invalid input "roomInfo"');
+  }
+});
+const sortedRegisteredData = computed(() => registeredData.value.sort((a: any, b: any) => (a.id < b.id ? 1 : -1)));
 
-function chat() {
-  axios
-    .post("/api/chatwork_post_message", {
-      room_id: roomId.value,
-      body: body.value,
-    })
-    .catch((err) => {
-      throw err;
-    });
-}
-function addDB() {
-  axios
-    .post("/api/db_add", {
-      room_id: roomId.value,
-      body: body.value,
-    })
-    .catch((err) => {
-      throw err;
-    });
-}
-
-async function getRegisteredData() {
+async function updateRegisteredData() {
   const dataArray: RegisteredData[] = [];
   await axios
     .get("/api/db_get_all")
@@ -58,29 +57,47 @@ async function getRegisteredData() {
     .catch((err) => {
       throw err;
     });
-  return dataArray;
+  registeredData.value = dataArray;
 }
 
-registeredData.value = await getRegisteredData();
+function register() {
+  axios
+    .post("/api/db_add", {
+      room_id: roomId.value,
+      body: inputData.value.body,
+      self_unread: inputData.value.selfUnread,
+    })
+    .then(() => {
+      updateRegisteredData();
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+// 表示するための情報を集める
+updateRegisteredData();
 </script>
 
 <template>
   <div>
-    <input v-model="roomUrl" />
-    <textarea v-model="body"></textarea>
-    <button @click="chat()">chat</button>
-    <button @click="addDB()">addDB</button>
-
     <table>
       <thead>
         <tr>
           <th>チャット部屋ID</th>
           <th>投稿予定文</th>
           <th>投稿者にとっても未読にするか</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="d in registeredData" :key="d.id">
+        <tr>
+          <td><input v-model="inputData.roomInfo" /></td>
+          <td><textarea v-model="inputData.body"></textarea></td>
+          <td><input type="checkbox" v-model="inputData.selfUnread" /></td>
+          <td><button @click="register">新規登録</button></td>
+        </tr>
+        <tr v-for="d in sortedRegisteredData" :key="d.id">
           <td>{{ d.roomId }}</td>
           <td>{{ d.body }}</td>
           <td>{{ d.selfUnread }}</td>
