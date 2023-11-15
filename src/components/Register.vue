286bfa84e5712fc4ac53a22c267285eca5f65053
s-watanabe <s-watanabe@three-rings.net>
2023-11-15 12:12:45 +0900

    登録済みデータを更新できるように

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -11,42 +11,44 @@ interface InputData {
 }
 interface RegisteredData {
   id: number;
-  roomId: number;
-  body: string;
-  selfUnread: boolean;
+  editableData: InputData;
+  editing: boolean;
 }
 
-const inputData = ref<InputData>({
+const newInputData = ref<InputData>({
   roomInfo: "https://www.chatwork.com/#!rid335028121",
   body: import.meta.env.VITE_APP_TITLE,
   selfUnread: false,
 });
 const registeredData = ref<RegisteredData[]>([]);
 
-const roomId = computed((): number => {
-  const roomInfo = inputData.value.roomInfo;
-  if (roomInfo.startsWith("https://www.chatwork.com/#!rid")) {
-    // チャット部屋のURLを指定していた場合はID部分を抽出
-    return Number(roomInfo.replace(/.*rid([0-9]+)$/, "$1"));
-  } else if (!roomInfo.match(/[^0-9]+/)) {
-    // 部屋IDを直接指定していたらそのまま扱う
-    return Number(roomInfo);
-  } else {
-    throw new Error('Invalid input "roomInfo"');
-  }
-});
 const sortedRegisteredData = computed(() => registeredData.value.sort((a: any, b: any) => (a.id < b.id ? 1 : -1)));
 
 async function updateViewData() {
   registeredData.value = await getRegisteredData();
 }
 
+function getRoomId(roomInfo: string | number): number {
+  if (typeof roomInfo === "number") {
+    // 数値の場合はそのまま扱う
+    return roomInfo;
+  } else if (roomInfo.startsWith("https://www.chatwork.com/#!rid")) {
+    // チャット部屋のURLを指定していた場合はID部分を抽出
+    return Number(roomInfo.replace(/.*rid([0-9]+)$/, "$1"));
+  } else if (!roomInfo.match(/[^0-9]+/)) {
+    // 部屋IDを直接文字列指定していたらそのまま扱う
+    return Number(roomInfo);
+  } else {
+    throw new Error('Invalid input "roomInfo"');
+  }
+}
+
 function register() {
   axios
     .post("/api/db_register", {
-      room_id: roomId.value,
-      body: inputData.value.body,
-      self_unread: inputData.value.selfUnread,
+      room_id: getRoomId(newInputData.value.roomInfo),
+      body: newInputData.value.body,
+      self_unread: newInputData.value.selfUnread,
     })
     .then(() => {
       updateViewData();
@@ -66,9 +68,12 @@ async function getRegisteredData(): Promise<RegisteredData[]> {
         data.forEach((x: any) => {
           dataArray.push({
             id: x.id,
-            roomId: x.room_id,
-            body: x.body,
-            selfUnread: x.self_unread,
+            editableData: {
+              roomInfo: x.room_id,
+              body: x.body,
+              selfUnread: x.self_unread,
+            },
+            editing: false,
           });
         });
       }
@@ -83,9 +88,9 @@ function updateRegisteredData(data: RegisteredData) {
   axios
     .put("/api/db_register", {
       id: data.id,
-      room_id: data.roomId,
-      body: data.body,
-      self_unread: data.selfUnread,
+      room_id: getRoomId(data.editableData.roomInfo),
+      body: data.editableData.body,
+      self_unread: data.editableData.selfUnread,
     })
     .then(() => {
       updateViewData();
@@ -128,17 +133,22 @@ updateViewData();
       </thead>
       <tbody>
         <tr>
-          <td><input v-model="inputData.roomInfo" /></td>
-          <td><textarea v-model="inputData.body"></textarea></td>
-          <td><input type="checkbox" v-model="inputData.selfUnread" /></td>
+          <td><input v-model="newInputData.roomInfo" /></td>
+          <td><textarea v-model="newInputData.body"></textarea></td>
+          <td><input type="checkbox" v-model="newInputData.selfUnread" /></td>
           <td><button @click="register">新規登録</button></td>
         </tr>
         <tr v-for="d in sortedRegisteredData" :key="d.id">
-          <td>{{ d.roomId }}</td>
-          <td>{{ d.body }}</td>
-          <td>{{ d.selfUnread }}</td>
-          <td><button @click="updateRegisteredData(d)">更新</button></td>
-          <td><button @click="deleteRegisteredData(d.id)">削除</button></td>
+          <td v-if="d.editing"><input v-model="d.editableData.roomInfo" /></td>
+          <td v-else>{{ d.editableData.roomInfo }}</td>
+          <td v-if="d.editing"><textarea v-model="d.editableData.body"></textarea></td>
+          <td v-else>{{ d.editableData.body }}</td>
+          <td v-if="d.editing"><input type="checkbox" v-model="d.editableData.selfUnread" /></td>
+          <td v-else>{{ d.editableData.selfUnread }}</td>
+          <td v-if="d.editing"><button @click="updateRegisteredData(d)">更新</button></td>
+          <td v-else><button @click="d.editing = true">編集</button></td>
+          <td v-if="d.editing"><button @click="d.editing = false">キャンセル</button></td>
+          <td v-else><button @click="deleteRegisteredData(d.id)">削除</button></td>
         </tr>
       </tbody>
     </table>
