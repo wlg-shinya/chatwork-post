337288b20490b7499e61de12ef9a18ac274bb204
s-watanabe <s-watanabe@three-rings.net>
2023-11-24 18:56:08 +0900

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,14 @@ import App from "./App.vue";
 import axios from "axios";
 axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}:${import.meta.env.VITE_BACKEND_PORT}`;
 
-createApp(App).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
