91093b549fca170fcc9bbfae012f23a1c782f45e
s-watanabe <s-watanabe@three-rings.net>
2023-11-29 15:31:26 +0900

    登録情報削除にトーストを経由するように

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, computed, watch, watchEffect } from "vue";
 import axios from "axios";
+import { Toast } from "bootstrap";
 import { RegisteredData, RegisteredDataUserInput } from "../types";
 import { Condition, concreteCondition, restoreCondition } from "../condition";
 import ConditionComponent from "./Condition.vue";
@@ -257,6 +258,23 @@ function cancelEdit(data: WorkingData) {
   // 登録済み情報を更新することで編集したデータを破棄
   updateWorkingData();
 }
+
+function notifyWarning(delegate: Function) {
+  const toastEl = document.getElementById("toastWarning");
+  if (!toastEl) return;
+  const toast = Toast.getOrCreateInstance(toastEl);
+  const buttonEl = document.getElementById("delete");
+  if (!buttonEl) return;
+
+  // 指定関数の登録
+  buttonEl.addEventListener("click", () => {
+    delegate();
+    toast.hide(); // 実行後はトーストを閉じる
+  });
+
+  // トースト表示
+  toast.show();
+}
 </script>
 
 <template>
@@ -320,12 +338,24 @@ function cancelEdit(data: WorkingData) {
               <ConditionComponent :condition="d.editableData.postCondition.class" :editting="d.editing" />
             </td>
             <td><button @click="startEdit(d)" class="btn btn-primary">編集</button></td>
-            <td><button @click="deleteRegisteredData(d.id)" class="btn btn-danger">削除</button></td>
+            <td><button @click="notifyWarning(() => deleteRegisteredData(d.id))" class="btn btn-danger">削除</button></td>
           </template>
         </tr>
       </tbody>
     </table>
   </template>
+  <div class="position-fixed top-50 start-50 translate-middle">
+    <div id="toastWarning" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <span class="me-auto fw-bold">注意</span>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body">この操作は取り消せません。本当に削除しますか？</div>
+      <div class="d-flex justify-content-center">
+        <button id="delete" class="btn btn-danger">削除</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
