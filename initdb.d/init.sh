dc89855e98974fa16cf33915facb6ad04195b529
s-watanabe <s-watanabe@three-rings.net>
2023-12-05 14:27:26 +0900

    ファイル名整理

--- initdb.d/init.sh
+++ initdb.d/init.sh
@@ -3,5 +3,5 @@ set -e
 dbname=chatpost
 
 psql -c "CREATE DATABASE ${dbname};"
-psql -d ${dbname} -f /docker-entrypoint-initdb.d/${dbname}.ddl
-psql -d ${dbname} -f /docker-entrypoint-initdb.d/${dbname}.dml
+psql -d ${dbname} -f /docker-entrypoint-initdb.d/ddl
+psql -d ${dbname} -f /docker-entrypoint-initdb.d/dml
